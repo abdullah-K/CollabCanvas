14f7c76cd091a3bcf1f5307ee4c1c96e96f00ef8
Abdullah <abdullahkhan0503@gmail.com>
2017-09-11 14:39:25 +0000

    Added files

---
+++ client/sketch.js
@@ -0,0 +1,67 @@
+// /client/sketch.js 
+/* global createCanvas,windowWidth,windowHeight,background,io,fill,noStroke,ellipse,clear,mouseX,mouseY,resizeCanvas*/
+
+
+
+//store the canvas in a variable
+var canvas;
+//keeps track of socket.io connections
+var socket;
+
+// setup canvas and receive mouse coordinates from ser
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  background(35);
+
+  socket = io.connect('https://collab-canvas-project-abdullah-k.c9users.io/');
+  //Socket event called 'mouse' created + make an anonymous callback function to draw the lines on other client's canvases
+  socket.on('mouse',
+    function(coordinates) {
+      fill(255, 32, 52);
+      noStroke();
+      //create ellipses on the coordinates being received from the server
+      ellipse(coordinates.x, coordinates.y, 17, 17);
+    }
+  );
+}
+
+//prevent page scroll and bounce effect for iOS Safari users
+document.ontouchmove = function(event){
+    event.preventDefault();
+}
+
+function draw() {
+  //empty
+}
+
+// clear the canvas (this function is called when the user presses the 'clear' button)
+function clearCanvas(){
+  clear();
+  background(35);
+};
+
+//function to send mouse coordinates to multiple clients
+function mouseDragged() {
+  //console.log('sending coordinates: ' + mouseX + ', ' + mouseY);
+  fill(235);
+  noStroke();
+  // draw ellipses on client's mouse position
+  ellipse(mouseX, mouseY, 17, 17);
+  
+  // store mouse coordinates in 'coordinates' object
+  var coordinates = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  //send data (mouse coordinates) to server
+  socket.emit('mouse', coordinates);
+}
+function mousePressed() {
+  mouseDragged();
+}
+
+// resize canvas when the browser window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+/* URL:  
+ * https://collab-canvas-project-abdullah-k.c9users.io/
+ */
+
+//dependencies:
+var express = require('express');
+var socket = require('socket.io');
+//define app as an express.js application
+var app = express();
+//specify server port with 'process.env.PORT'
+var server = app.listen(process.env.PORT);
+//keeps track of Input/Output
+var io = socket(server);
+//serves files from 'client' folder
+app.use(express.static('client'));
+//keeps track of usernames
+var users = [];
+//array to store number of connections to server
+var connections = [];
+
+//keeps a count of online users at any given moment
+var clientCount = 1;
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log('New connection detected! (connection id: ' + socket.id );
+  connections.push(socket);
+  socket.on('mouse', mouseLocation);
+  
+
+  function mouseLocation(coordinates) {
+    socket.broadcast.emit('mouse', coordinates);
+  }
+  
+  //display message when client disconnects
+  socket.on('disconnect', function() {
+      //  + "(" + socket.username + ")"
+      console.log("A client has disconnected");
+      users.splice(users.indexOf(socket.username), 1);
+      updateUserNames();
+      connections.splice(connections.indexOf(socket), 1);
+      //console.log("A client has disconnected");
+  });
+  
+  //new user
+  socket.on('newUser', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUserNames();
+    io.sockets.emit('clientsNum', clientCount++);
+  });
+  socket.on('userDisconnect', function() {
+    updateUserNames();
+    io.sockets.emit('clientsNum', clientCount--);
+  });
+  
+  //update user names function
+  function updateUserNames() {
+    io.sockets.emit('getUsers', users);
+    console.log("Userlist updated");
+  }
+}
+//test command
+console.log("Server is live!");
